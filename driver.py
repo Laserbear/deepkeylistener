e3ec8d17f0a818cc04e1cecd040ce9e0c2e22d57
Christian Ng <christianr.ng@gmail.com>
2016-07-25 00:40:30 -0700

    initialize tf variables

--- driver.py
+++ driver.py
@@ -26,3 +26,8 @@ pred = BiRNN(x, weights, biases)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.initialize_all_variables()
+
