5e25398c8d16601fb54d000545aac90c42d63371
Christian Ng <christianr.ng@gmail.com>
2016-07-28 00:34:27 -0700

    flesh out data handler

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,7 @@
 from mffc_converter import convert
-
+counter = 1
 def pull_data_chunk():
 	#hard code data path, i guess
-	#call that 
+	global counter #gross syntax but necessary
+	path = "data/audio"
+	return convert(path+str(counter)+".wav")
