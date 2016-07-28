4a6313863de811304f01853e5b829676dee5dc34
Christian Ng <christianr.ng@gmail.com>
2016-07-28 00:35:36 -0700

    fix increment bug

--- data_handler.py
+++ data_handler.py
@@ -3,5 +3,6 @@ counter = 1
 def pull_data_chunk():
 	#hard code data path, i guess
 	global counter #gross syntax but necessary
-	path = "data/audio"
-	return convert(path+str(counter)+".wav")
+	path = "data/audio"+str(counter)+".wav"
+	counter += 1
+	return convert(path)
